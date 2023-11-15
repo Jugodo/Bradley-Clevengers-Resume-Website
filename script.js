abd947deef44572d7ee4998017e2ceff2e824b0d
Bradley Clevenger <120070364+Jugodo@users.noreply.github.com>
2023-11-15 12:09:14 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-const scrollProgressElement = document.querySelector
-('#scroll-progress');
-
-function scrollProgress() {
-    const totalheightOfWebPage = document.body.scrollheight;
-    const currentDistanceFromTop = document.documentElement.
-    scrollTop;
-
-    const windowHeight = document.documentElement.clientHeight;
-const scrolledPercentage = (cuurrentDistanceFromTop / (totalheightOfWebPage - windowHeight)) *100;
-
-scrollProgressElement.computedStyleMap.width = Math.round(scrolledPercentage) + "%";  
-
-}
-
-document.addEventListener('scroll', scrollProgress);
+const d = new Date();
